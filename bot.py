2d5b170260a8a4c03dd7b7f06f829d614a831615
James Marcos <macosveloso2019@gmail.com>
2018-11-05 14:28:32 -0300

    bot incompleto

---
+++ bot.py
@@ -0,0 +1,108 @@
+import discord
+
+
+client = discord.Client()
+
+TOKEN = "Coloque o token do seu bot aqui"
+
+
+players = {}
+COR = 0xF7FE2E
+
+@client.event
+async def on_ready():
+    print(client.user.name)
+    print("===================")
+
+@client.event
+async def on_message(message):
+    if message.content.lower().startswith('!entrar'):
+        try:
+            channel = message.author.voice.voice_channel
+            await client.join_voice_channel(channel)
+        except discord.errors.InvalidArgument:
+            await client.send_message(message.channel, "O bot ja esta em um canal de voz")
+        except Exception as error:
+            await client.send_message(message.channel, "Ein Error: ```{error}```".format(error=error))
+
+    if message.content.lower().startswith('!sair'):
+        try:
+            mscleave = discord.Embed(
+                title="\n",
+                color=COR,
+                description="Sai do canal de voz e a musica parou!"
+            )
+            voice_client = client.voice_client_in(message.server)
+            await client.send_message(message.channel, embed=mscleave)
+            await voice_client.disconnect()
+        except AttributeError:
+            await client.send_message(message.channel, "O bot não esta em nenhum canal de voz.")
+        except Exception as Erro:
+            await client.send_message(message.channel, "Ein Error: ```{haus}```".format(haus=Erro))
+
+    if message.content.lower().startswith('!play'):
+        try:
+            yt_url = message.content[6:]
+            if client.is_voice_connected(message.server):
+                try:
+                    voice = client.voice_client_in(message.server)
+                    players[message.server.id].stop()
+                    player = await voice.create_ytdl_player('ytsearch: {}'.format(yt_url))
+                    players[message.server.id] = player
+                    player.start()
+                    mscemb = discord.Embed(
+                        title="Música para tocar:",
+                        color=COR
+                    )
+                    mscemb.add_field(name="Nome:", value="`{}`".format(player.title))
+                    
+                except Exception as e:
+                    await client.send_message(message.server, "Error: [{error}]".format(error=e))
+
+            if not client.is_voice_connected(message.server):
+                try:
+                    channel = message.author.voice.voice_channel
+                    voice = await client.join_voice_channel(channel)
+                    player = await voice.create_ytdl_player('ytsearch: {}'.format(yt_url))
+                    players[message.server.id] = player
+                    player.start()
+                    mscemb2 = discord.Embed(
+                        title="Música para tocar:",
+                        color=COR
+                    )
+                    mscemb2.add_field(name="Nome:", value="`{}`".format(player.title))
+                    
+                    await client.send_message(message.channel, embed=mscemb2)
+                except Exception as error:
+                    await client.send_message(message.channel, "Error: [{error}]".format(error=error))
+        except Exception as e:
+            await client.send_message(message.channel, "Error: [{error}]".format(error=e))
+
+
+
+
+    if message.content.lower().startswith('!pause'):
+        try:
+            mscpause = discord.Embed(
+                title="\n",
+                color=COR,
+                description="Musica pausada com sucesso!"
+            )
+            await client.send_message(message.channel, embed=mscpause)
+            players[message.server.id].pause()
+        except Exception as error:
+            await client.send_message(message.channel, "Error: [{error}]".format(error=error))
+    if message.content.startswith('!resume'):
+        try:
+            mscresume = discord.Embed(
+                title="\n",
+                color=COR,
+                description="Musica pausada com sucesso!"
+            )
+            await client.send_message(message.channel, embed=mscresume)
+            players[message.server.id].resume()
+        except Exception as error:
+            await client.send_message(message.channel, "Error: [{error}]".format(error=error))
+
+
+client.run(TOKEN)
